3686d2bf5571ebf39999e837e4e32d909df4d05f
joão <joaoaaraujosilva2022@gmail.com>
2026-09-01 11:52:26 -0300

    feat: funcionalidades de tarefas

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import '../css/estilo.css'
 
 const Tarefas = () => {
 
@@ -14,10 +15,53 @@ const Tarefas = () => {
         localStorage.setItem("i.tem-tarefa", JSON.stringify(tarefas))
     }, [tarefas])
 
+    const Adicionartarefa =(e)=>{
+      //  Previne que a pagina se recarregue automaticamente
+      e.preventDefault();
+      // Valida se o campo estiver vazio
+      if(!campo.trim()) return;
+
+      // Novo objeto 
+      const novaTarefa={
+        id: Date.now(),
+        texto: campo,
+      }
+      setTarefas([...tarefas,novaTarefa]);
+      setcampo('');
+    }
+
+    const RemoverTarefas=(id)=>{
+      //  Verifica se o D da tarefa atual é diferente do id que deseja apagar
+      // Se o ID for igual(tarefa que deseja apagar) a condição retorna falso
+      // e o item é excluido
+      const apagarTarefa = tarefas.filter((tarefa)=> tarefa.id !== id)
+      setTarefas(apagarTarefa);
+    }
+
   return (
-    <>
-      
-    </>
+    <div className='todo-container'>
+      <h1>Minha Lista de Tarefas</h1>
+      <form onSubmit={Adicionartarefa}>
+        <input
+          type="text"
+          value={campo}
+          onChange={(e)=>setcampo(e.target.value)}
+          placeholder='Digite sua Tarefa'
+          className="todo-input"
+        />
+        <button type='submit'>Adicionar</button>
+      </form>
+
+      <ul>
+        {tarefas.map((tarefa)=>(
+          <li key={tarefa.id}>
+            <span>{tarefa.texto}</span>
+            <button onClick={()=>RemoverTarefas(tarefa.id)}>Excluir</button>
+          </li>
+        ))}
+      </ul>
+      {tarefas.length === 0 && <p>Nenhuma tarefa salva</p>}
+    </div>
   )
 }
 
