06c36865b4d60030bf25f5503e5497863ebb34cb
joão <joaoaaraujosilva2022@gmail.com>
2026-09-08 11:31:20 -0300

    sytle: Estilização doi projeto com tailwindcss

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -39,28 +39,30 @@ const Tarefas = () => {
     }
 
   return (
-    <div className='todo-container'>
-      <h1>Minha Lista de Tarefas</h1>
-      <form onSubmit={Adicionartarefa}>
+    <div className="max-w-md mx-auto mt-10 bg-indigo-500 rounded-2xl shadow shadow-red-600 border-amber-500">
+      <h1 className='text-2xl font-bold text-white mb-6 text-center'>Minha Lista de Tarefas</h1>
+      <form onSubmit={Adicionartarefa} className='flex gap-2 mb-6'>
         <input
           type="text"
           value={campo}
           onChange={(e)=>setcampo(e.target.value)}
           placeholder='Digite sua Tarefa'
-          className="todo-input"
+          className="flex-1 px-4 py-2 border-gray-700 rounded 2xl focus:outline-none focus:ring-1 focus:ring-amber-200 focus:border-transparent text-black placeholder:text-gray-700"
         />
-        <button type='submit'>Adicionar</button>
+        <button type='submit'
+          className='bg-indigo-950 hover:bg-indigo-400 text-white font-medium px-5 rounded-2xl trasition-colors cursor-pointer'
+        >Adicionar</button>
       </form>
 
-      <ul>
+      <ul className='space-y-3'>
         {tarefas.map((tarefa)=>(
-          <li key={tarefa.id}>
+          <li key={tarefa.id} className='flex intems-center justify-between p-3 bg-indigo-900 border border-amber-300 rounded-2xl shadow-sm hover:bg-indigo-500 trasition-colors text-white'>
             <span>{tarefa.texto}</span>
-            <button onClick={()=>RemoverTarefas(tarefa.id)}>Excluir</button>
+            <button onClick={() => RemoverTarefas(tarefa.id)} className='bg-red-950 hover:bg-indigo-400 text-white font-medium px-5 rounded-2xl trasition-colors cursor-pointer'>Excluir</button>
           </li>
         ))}
       </ul>
-      {tarefas.length === 0 && <p>Nenhuma tarefa salva</p>}
+      {tarefas.length === 0 && <p className='text-center italic mt-4 text-white'>Nenhuma tarefa salva</p>}
     </div>
   )
 }
